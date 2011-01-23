6ede144941218aa081a35f09d71a7f87b054a223
holtonma <holtonma@gmail.com>
2011-01-23 15:28:42 -0800

    slot machine in index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interview::Application.config.secret_token = 'c59aeec8dbdd55944a1d536f7d0fca0cfb415457694b15e3e79770dab4d7d3d208dc8bcbfc55194fc480d9f376361f254ccbbe69df36055cc751a87dc6216e4d'
